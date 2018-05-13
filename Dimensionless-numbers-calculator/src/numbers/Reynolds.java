b430c3fe5d1b7c78c9745511511f36f39f1516b3
FluidMechanicsAndMore <sseba01@gmail.com>
2018-05-13 12:49:11 +0200

    Added Reynolds number

--- Dimensionless-numbers-calculator/src/numbers/Reynolds.java
+++ Dimensionless-numbers-calculator/src/numbers/Reynolds.java
@@ -1,5 +1,44 @@
 package numbers;
 
-public class Reynolds {
+    public class Reynolds {
 
-}
+        public double calculateReynoldsWithDynamicViscosity(double density, double velocity, double dynamicViscosity, double linearDimension){
+            double Re;
+            Re = (density*velocity*linearDimension)/dynamicViscosity;
+
+            return Re;
+        }
+
+        public double calculateReynoldsWithKinematicViscosity(double velocity, double kinematicViscosity, double linearDimension){
+            double Re;
+            Re = velocity*linearDimension/kinematicViscosity;
+
+            return Re;
+        }
+
+        public double velocityWithDynamicViscosity(double Re, double density, double dynamicViscosity, double linearDimension) {
+
+            return Re*dynamicViscosity/(density*linearDimension);
+        }
+
+        public double velocityWithKinematicViscosity(double Re, double kinematicViscosity, double linearDimension){
+
+            return Re * kinematicViscosity / linearDimension;
+        }
+
+        public double linearDimensionWithDynamicViscosity( double Re, double density, double velocity, double dynamicViscosity){
+
+            return Re * dynamicViscosity / ( velocity*density);
+        }
+
+        public double linearDimensionWithKinematicViscosity(double Re, double velocity, double kinematicViscosity) {
+
+            return Re*kinematicViscosity/velocity;
+        }
+
+        public double densityWithDynamicViscosity(double Re, double velocity, double dynamicViscosity, double linearDimension){
+
+            return Re*dynamicViscosity / (velocity*linearDimension);
+        }
+
+    }
